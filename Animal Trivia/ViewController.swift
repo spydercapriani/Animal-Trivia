f9bdbb893007d744af765d759228e77969f193db
Danny Gilbert <gilbert.daniel@heb.com>
2016-01-06 10:25:51 -0600

    Initial Commit

---
+++ Animal Trivia/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Animal Trivia
+//
+//  Created by Daniel Gilbert on 1/6/16.
+//  Copyright © 2016 Daniel Gilbert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
